021f5157c1bc854b5657f96eb814f45d6878f862
BattleWoofer <77591232+BattleWoofer@users.noreply.github.com>
2023-04-26 13:13:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-
-console.log("asa")
-
 var player = {
     t1: new Decimal(1),
     c1: new Decimal(10),
     x: new Decimal(10)
 };
-
-
+/*
 setInterval(function(a){
     gain(0.1);
     document.getElementById("test").innerHTML = player.t1.normify(2);
@@ -17,4 +13,4 @@ function gain(v){
     g = new Decimal(v)
     g = g.mul(player.t1)
     player.t1 = player.t1.mul(g)
-}
+} */
