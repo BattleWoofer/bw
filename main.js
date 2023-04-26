1a5344abd18dd287c23ca56a6b3a61ef1052c3d0
BattleWoofer <omeet100@gmail.com>
2023-04-26 07:28:58 -0500

    ds

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
+
 console.log("asa")
 
-const x = new Decimal(123.4567);
-console.log(x.plus(1))
+var x = new Decimal(0);
+var player = {
+    t1: 0,
+};
+
+
+setInterval(function(a){
+    console.log("bleh")
+    gain(0.1);
+    document.getElementById("test").innerHTML = x.normify(2);
+},100)
+
+function gain(v){
+    g = new Decimal(v)
+    g = g.mul(player.t1)
+    x = x.add(g)
+}
