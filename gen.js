08e9b9e00f97f4eb9dcee155da052c4e6ff5c693
BattleWoofer <77591232+BattleWoofer@users.noreply.github.com>
2023-04-26 12:30:48 -0500

    Update gen.js

--- gen.js
+++ gen.js
@@ -9,5 +9,6 @@ document.getElementById("t1buy").addEventListener("click", function(z){
 function cost1(){
  ct1 = new Decimal(10);
  ct1 = ct1.mul((new Decimal(1.5)).pow(player.t1));
+ ct1 = ct1.mul(Math.pow(1.5, player.t1))
  player.c1 = ct1;
 }
