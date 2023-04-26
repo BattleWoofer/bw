e70f94120f97f24dc4dfc75db3c37e30c0a9942d
BattleWoofer <77591232+BattleWoofer@users.noreply.github.com>
2023-04-26 10:11:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 
 console.log("asa")
 
-var x = new Decimal(0);
 var player = {
     t1: 0,
+    x: new Decimal(0)
 };
 
 
 setInterval(function(a){
     console.log("bleh")
     gain(0.1);
-    document.getElementById("test").innerHTML = x.normify(2);
+    document.getElementById("test").innerHTML = player.x.normify(2);
 },100)
 
 function gain(v){
     g = new Decimal(v)
     g = g.mul(player.t1)
-    x = x.add(g)
+    player.x = player.x.add(g)
 }
