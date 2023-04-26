9c65fc154988d25e918353fc89f0594655090677
BattleWoofer <77591232+BattleWoofer@users.noreply.github.com>
2023-04-26 12:23:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,12 +10,11 @@ var player = {
 
 setInterval(function(a){
     gain(0.1);
-    document.getElementById("test").innerHTML = player.c1.normify(2);
+    document.getElementById("test").innerHTML = player.t1.normify(2);
 },100)
 
 function gain(v){
     g = new Decimal(v)
     g = g.mul(player.t1)
-    console.log(player.c1)
-   // player.c1 = player.c1.plus(g)
+   // player.t1 = player.t1.plus(g)
 }
